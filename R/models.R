72b9bb534e48894f0576a8d45bf69cd303a33bce
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-09-17 04:37:28 +0200

    feat: implement proper data prep

--- R/models.R
+++ R/models.R
@@ -4,7 +4,33 @@
 #' @param df The input data frame
 #' @return A data frame with only the feature columns
 df_feat <- function(df) {
-    df |> select(-aggr_activity, -activity_confidence)
+    df |> select(-any_of(c("aggr_activity", "activity_confidence")))
+}
+
+fit_preprocess <- function(df) {
+    caret::preProcess(df_feat(df), method = c("nzv", "corr", "center", "scale"))
+}
+
+apply_preprocess <- function(df, pp) {
+    predict(pp, newdata = df_feat(df))
+}
+
+make_data <- function(df, key, pp_utils, inc_outcome = FALSE) {
+    x <- apply_preprocess(df, pp_utils$preproc)
+
+    if (grepl("km_cluster", key)) {
+        x <- x |> mutate(cluster = get_kmeans_clusters(x, pp_utils$km))
+    } else if (grepl("km_dist", key)) {
+        x <- x |> bind_cols(get_kmeans_dist_df(x, pp_utils$km))
+    }
+
+    x <- x |> mutate(weights = rep(1, nrow(x)))
+
+    if (inc_outcome) {
+        x <- x |> mutate(aggr_activity = df$aggr_activity)
+    }
+
+    return(x)
 }
 
 #' Get a data frame with feature cols and additional
@@ -13,24 +39,23 @@ df_feat <- function(df) {
 #' @param km The k-means object
 #' @return A data frame with the k-means distances
 get_kmeans_dist_df <- function(df, km) {
-    dists <- proxy::dist(df_feat(df), km$centers)
-    df_km_dists <- unclass(dists) |>
-        data.frame() |>
-        mutate(aggr_activity = df$aggr_activity)
+    dists <- proxy::dist(as.matrix(df), as.matrix(km$centers))
+    df_km_dists <- unclass(dists) |> data.frame()
+    # Change col names to start with "km_dist_"
+    names(df_km_dists) <- paste0("km_dist_", names(df_km_dists))
     return(df_km_dists)
 }
 
-#' Get a data frame with feature cols and additional
-#' col for the k-means cluster assignment
+#' Get the nearest k-means cluster for each data point
 #' @param df The input data frame
 #' @param km The k-means object
-#' @return A data frame with the k-means cluster assignments
-get_kmeans_df <- function(df, km) {
-    df_km <- data.frame(
-        cluster = factor(km$cluster),
-        aggr_activity = df$aggr_activity
-    )
-    return(df_km)
+#' @return A vector with the nearest cluster for each data point
+get_kmeans_clusters <- function(df, km) {
+    # ==> START LLM https://chatgpt.com/share/6aab3c44-4174-83eb-b07d-5f27ba5ca1bb
+    dists <- get_kmeans_dist_df(df, km)
+    nearest_cluster <- max.col(-dists, ties.method = "first")
+    return(nearest_cluster)
+    # ==> END LLM
 }
 
 #' Define options for model training, which are used to
@@ -48,24 +73,33 @@ define_train_options <- function() {
     return(opts)
 }
 
+fit_preprocess <- function(df) {
+    caret::preProcess(
+        df_feat(df),
+        method = c("nzv", "corr", "center", "scale")
+    )
+}
+
 #' Construct a list of specifications for fitting a model
 #' based on provided options
 #' @param df The input data frame
-#' @param df_km The data frame with k-means cluster assignments
-#' @param df_km_dists The data frame with k-means distances
+#' @param pp_utils The list of preprocessing utilities
 #' @param method The method for training
 #' @param term_mode The term mode option
 #' @param term The term option
 #' @param weighting The weighting option
 #' @return A list of specifications for fitting a model
 get_fit_spec <- function(
-    df, df_km, df_km_dists,
+    df, pp_utils,
     method, term_mode, term, weighting
 ) {
     key <- paste(method, term_mode, term, weighting, sep = "_")
 
-    data <- df_feat(df) |>
-        mutate(aggr_activity = df$aggr_activity) |>
+    # Add terms
+    data <- make_data(df, key, pp_utils, TRUE)
+
+    # Add weights
+    data <- data |>
         mutate(
             weights = if (weighting == "weighted") {
                 df$activity_confidence
@@ -74,20 +108,12 @@ get_fit_spec <- function(
             }
         )
 
-    # add terms
-    data <- switch(term,
-        "noterm" = data,
-        "km_cluster" = data |> bind_cols(cluster = df_km$cluster),
-        "km_dist" = data |> bind_cols(df_km_dists |> select(-aggr_activity)),
-        data
-    )
-
-    # add term mode
+    # Create formula
     if (term_mode == "inter" && term == "km_cluster") {
         formula <- aggr_activity ~ (. - weights) * cluster
     } else if (term_mode == "inter" && term == "km_dist") {
-        feat_names <- names(df_feat(df))
-        dist_names <- names(df_km_dists |> select(-aggr_activity))
+        feat_names <- names(apply_preprocess(df, pp_utils$preproc))
+        dist_names <- grep("^km_dist_", names(data), value = TRUE)
         # ==> START LLM https://chatgpt.com/share/6aaab81f-4d5c-83eb-bfac-021950015cd3
         rhs <- paste0(
             "(", paste(feat_names, collapse = " + "), ") * ",
@@ -112,11 +138,13 @@ fit_models <- function(df, number = 2, repeats = 1, nstart = 2) {
     # Define train control with repeated cross-validation
     trcntr <- caret::trainControl(method = "repeatedcv", number = number, repeats = repeats, verboseIter = FALSE)
 
-    # Precompute relevant data (with additional features)
+    # Precompute relevant data
+    pp <- fit_preprocess(df)
+    df_pp <- apply_preprocess(df, pp)
     k <- length(unique(df$aggr_activity))
-    km_fit <- kmeans(df_feat(df), centers = k, nstart = nstart)
-    df_km <- get_kmeans_df(df, km_fit)
-    df_km_dists <- get_kmeans_dist_df(df, km_fit)
+    km <- kmeans(df_pp, centers = k, nstart = nstart)
+
+    pp_utils = list(preproc = pp, km = km)
 
     # Define options for training
     opts <- define_train_options()
@@ -124,7 +152,7 @@ fit_models <- function(df, number = 2, repeats = 1, nstart = 2) {
     # Get specifications for training models
     specs <- pmap(opts, \(method, term_mode, term, weighting) {
             get_fit_spec(
-                df, df_km, df_km_dists,
+                df, pp_utils,
                 method, term_mode, term, weighting
             )
         }
@@ -132,15 +160,15 @@ fit_models <- function(df, number = 2, repeats = 1, nstart = 2) {
 
     # Fit the models
     models <- list()
+    i <- 1
     for (spec in specs) {
-        cat("Fitting model:", spec$key, "...\n")
+        cat("Fitting model", i, "/", length(specs), ":", spec$key, "...\n")
         model <- caret::train(
             spec$formula,
             data = spec$data,
             weights = weights,
             method = spec$method,
             trControl = trcntr,
-            preProcess = c("nzv", "corr", "center", "scale"),
             MaxNWts = 10000,
             maxit = 1000,
             trace = FALSE
@@ -148,7 +176,7 @@ fit_models <- function(df, number = 2, repeats = 1, nstart = 2) {
         models[[spec$key]] <- model
     }
 
-    return(models)
+    return(list(models = models, pp_utils = pp_utils))
 }
 
 #' Fit all models and return the results
@@ -159,12 +187,14 @@ fit_models <- function(df, number = 2, repeats = 1, nstart = 2) {
 #' @param nstart The number of random starts for k-means
 #' @return A list of fitted models and their results
 fit_all <- function(df, number = 2, repeats = 1, nstart = 2) {
-    models <- fit_models(df, number, repeats, nstart)
+    fitted_models <- fit_models(df, number, repeats, nstart)
+    models <- fitted_models$models
+    pp_utils <- fitted_models$pp_utils
 
     results <- data.frame(
         accuracy = sapply(models, function(x) max(x$results$Accuracy)),
         kappa = sapply(models, function(x) max(x$results$Kappa))
     )
 
-    return(list(models = models, results = results))
+    return(list(models = models, results = results, pp_utils = pp_utils))
 }
