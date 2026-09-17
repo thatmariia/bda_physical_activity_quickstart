306ca6ade91f6585b9fa2ff84a85f4742c276787
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-09-17 20:06:46 +0200

    refactor: move functions to relevant places in the notebook

--- build_notebook.R
+++ build_notebook.R
@@ -1,6 +1,14 @@
 # Converts notebook.Rmd to output/notebook.ipynb for Kaggle.
 # The code is not run here; Kaggle runs it.
 
+# Every file in R/ must be shown in one of the notebooks in analysis/
+analysis_text <- unlist(lapply(list.files(here::here("analysis"), full.names = TRUE), readLines))
+for (r_file in list.files(here::here("R"))) {
+  if (!any(grepl(paste0('"R", "', r_file, '"'), analysis_text, fixed = TRUE))) {
+    stop("R/", r_file, " is not included in any notebook in analysis/", call. = FALSE)
+  }
+}
+
 ipynb <- rmarkdown::output_format(
   knitr = rmarkdown::knitr_options(
     opts_chunk = list(eval = FALSE),
@@ -10,14 +18,33 @@ ipynb <- rmarkdown::output_format(
     }),
     knit_hooks = list(source = function(x, options) {
       code <- paste(x, collapse = "\n")
-      paste0("\n::: {.cell .code}\n```r\n", code, "\n```\n:::\n")
+      cell <- paste0("```r\n", code, "\n```\n:::\n")
+      if (is.null(options$fold)) {
+        return(paste0("\n::: {.cell .code}\n", cell))
+      }
+      # Folded chunks: a title, and a cell that is collapsed below
+      paste0(
+        "\n*", options$fold, "*\n\n",
+        "::: {.cell .code tags=\"[\\\"hide-input\\\"]\"}\n", cell
+      )
     })
   ),
   pandoc = rmarkdown::pandoc_options(to = "ipynb")
 )
 
-rmarkdown::render(
+notebook_file <- rmarkdown::render(
   here::here("notebook.Rmd"),
   output_format = ipynb,
   output_dir = here::here("output")
 )
+
+# Collapse the marked cells on Kaggle and in Jupyter
+notebook <- jsonlite::read_json(notebook_file)
+notebook$cells <- lapply(notebook$cells, function(cell) {
+  if ("hide-input" %in% unlist(cell$metadata$tags)) {
+    cell$metadata[["_kg_hide-input"]] <- TRUE
+    cell$metadata$jupyter <- list(source_hidden = TRUE)
+  }
+  cell
+})
+jsonlite::write_json(notebook, notebook_file, auto_unbox = TRUE, pretty = TRUE, null = "null")
