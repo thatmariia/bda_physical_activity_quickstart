08dc6b1589c211431720881c0aa4fb81f56b12be
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-09-19 15:58:50 +0200

    feat: combining all features together

--- R/combining.R
+++ R/combining.R
@@ -10,27 +10,35 @@
 #' @param n_samples_per_epoch The number of samples per epoch.
 #' @param sample_rate The sampling rate of the data.
 #' @return A data frame containing the extracted features.
-parse_and_features <- function(filename, dir, sample_labels, n_samples_per_epoch, sample_rate) {
-  # Parse and load the df
-  df <- load_and_parse_file(dir, filename, sample_labels)
-  df_freq <- convert_signal_to_spectrum_df(df, n_samples_per_epoch, sample_rate)
-
-  # Extract relevant time and freq features based on the sensor type
-  params <- get_file_params(filename)
-  if (params$sensor_type == "acc") {
-    # Process accelerometer data
-    features_time <- get_time_domain_features(df, n_samples_per_epoch, sample_rate)
-    features_freq <- get_frequency_domain_features(df_freq)
-  } else if (params$sensor_type == "gyro") {
-    # Process gyroscope data
-    features_time <- get_time_domain_features(df, n_samples_per_epoch, sample_rate)
-    features_freq <- get_frequency_domain_features(df_freq)
+parse_and_features <- function(filename_acc, dir, sample_labels, n_samples_per_epoch, sample_rate) {
+  # check that filename starts with "acc"
+  if (!grepl("^acc", filename_acc)) {
+    stop("File must start with 'acc'")
   }
+  filename_gyro <- sub("^acc", "gyro", filename_acc)
 
-  # Left join features by epoch
-  features <- left_join(features_time, features_freq, by = "epoch")
+  # Parse and load the dfs
+  df_acc <- load_and_parse_file(dir, filename_acc, sample_labels)
+  df_gyro <- load_and_parse_file(dir, filename_gyro, sample_labels)
+  df_freq_acc <- convert_signal_to_spectrum_df(df_acc, n_samples_per_epoch, sample_rate)
+  df_freq_gyro <- convert_signal_to_spectrum_df(df_gyro, n_samples_per_epoch, sample_rate)
+  joint_df <- join_sensors(df_acc, df_gyro)
+
+  # Extract features from each sensor
+  features_time_acc <- get_time_domain_features(df_acc, n_samples_per_epoch, sample_rate)
+  features_freq_acc <- get_frequency_domain_features(df_freq_acc)
+  features_time_gyro <- get_time_domain_features(df_gyro, n_samples_per_epoch, sample_rate)
+  features_freq_gyro <- get_frequency_domain_features(df_freq_gyro)
+  features_joint <- get_joint_features(joint_df, n_samples_per_epoch)
+
+  # Left join all the features
+  time_features <- left_join(features_time_acc, features_time_gyro, by = "epoch", suffix = c("_acc", "_gyro"))
+  freq_features <- left_join(features_freq_acc, features_freq_gyro, by = "epoch", suffix = c("_acc", "_gyro"))
+  sensor_features <- left_join(time_features, freq_features, by = "epoch", suffix = c("_time", "_freq"))
+  features <- left_join(sensor_features, features_joint, by = "epoch", suffix = c("", "_joint"))
 
   # Add cols for user_id and exp_id from params
+  params <- get_file_params(filename_acc)
   features <- features |> mutate(user_id = params$user_id, exp_id = params$exp_id)
 
   return(features)
